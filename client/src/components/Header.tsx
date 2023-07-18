ea15d00e74688fcfa24d7a50723ae8ece2f87459
CEN Smart <echinnannachukwuemeka@gmail.com>
2023-07-18 15:00:14 +0100

    new update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, Button, Center, Heading, Text, Flex } from '@chakra-ui/react'
+import { Box, Button, Grid, Heading, Text, Flex } from '@chakra-ui/react'
 import Logo from './Logo'
 
 const Header = () => {
@@ -10,22 +10,22 @@ const Header = () => {
           <Logo />
           <Button className='btn'>Sign In</Button>
         </Flex>
-        <Center className='w-3/4 mx-auto mt-[5%]'>
-          <Box className='text-zinc-50 text-center space-y-4'>
-            <Heading as='h1' className='font-bold' fontSize={{
-              base: '2xl',
-              md: '4xl',
+        <Grid className='w-4/5 mx-auto min-h-[70vh] place-items-center'>
+          <Box className='text-zinc-50 text-center space-y-6'>
+            <Heading as='h1' className='font-bold lg:w-[80%] mx-auto' fontSize={{
+              base: '4xl',
+              md: '5xl',
               lg: '6xl'
             }}>Discover local events that spark your passion</Heading>
-            <Text className=' w-[73%] mx-auto' fontSize={{
+            <Text className=' lg:w-[65%] mx-auto' fontSize={{
               base: 'sm',
               md: 'md',
-              lg: '2xl'
+              lg: 'lg'
             }}>Whether you’re looking for live music, art exhibits, food festivals, or sporting events, we’ve got you covered with our easy to use platform. You ca browse events by date, category, and location, and even purchase tickets right from our website.
               So why wait? Join us and start exploring today!</Text>
-            <Button className='btn mt-8'>Connect Wallet</Button>
+            <Button className='btn'>Connect Wallet</Button>
           </Box>
-        </Center>
+        </Grid>
       </Box>
     </>
   )
