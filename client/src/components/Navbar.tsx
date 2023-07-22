ca16ee2ea4e54c0609bab8f911a4edcc3961d695
Ebuka <ebuka2264@yahoo.com>
2023-07-22 23:21:27 +0100

    wallet

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,30 +3,16 @@ import Logo from './Logo'
 import Link from 'next/link'
 import { Box, Flex, Button } from '@chakra-ui/react'
 import ButtonType from './Button'
-import { EthereumClient } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { WagmiConfig, mainnet } from 'wagmi'
-import { arbitrum, polygon } from 'viem/dist/types/chains'
+import { useWeb3Modal } from '@web3modal/react'
 
-
-const chains = [arbitrum, mainnet, polygon]
-const projectId = process.env.WALLET_CONNECT_KEY
-
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-})
-const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 const Navbar = () => {
+  const { open, close } = useWeb3Modal()
 
 
   return (
     <>
       {/* @ts-ignore */}
-      <WagmiConfig config={wagmiConfig}>
 
         <Box as='header' className=' bg-black text-white fixed z-10 w-full px-2 md:px-4 xl:px-8 py-4 drop-shadow-xl border-b border-b-white'>
           <Flex className=' items-center justify-between text-md md:text-lg'>
@@ -39,6 +25,7 @@ const Navbar = () => {
               <ButtonType bgColor='bg-purple-700' label='Connect Wallet' border='purple' color='white' bgModified='purple.500' onClick={async () => {
                 console.log("clicked")
                 open()
+
               }} />
               {/* <Menu>
               <MenuButton>
@@ -55,31 +42,10 @@ const Navbar = () => {
             </Flex>
           </Flex>
         </Box>
-      </WagmiConfig>
 
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
 
     </>
   )
 }
 
 export default Navbar
-
-function configureChains(chains: any[], arg1: any[]): { publicClient: any } {
-  throw new Error('Function not implemented.')
-}
-
-
-function w3mProvider(arg0: { projectId: string }) {
-  throw new Error('Function not implemented.')
-}
-
-
-function createConfig(arg0: { autoConnect: boolean; connectors: any; publicClient: any }) {
-  throw new Error('Function not implemented.')
-}
-
-
-function w3mConnectors(arg0: { projectId: string; chains: any[] }) {
-  throw new Error('Function not implemented.')
-}
