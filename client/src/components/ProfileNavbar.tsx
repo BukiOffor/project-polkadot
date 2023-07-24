b66b7898728a9b2b3152b9ebee5a3966eea8d0c4
CEN Smart <echinnannachukwuemeka@gmail.com>
2023-07-24 12:39:39 +0100

    uploaded files

--- client/src/components/ProfileNavbar.tsx
+++ client/src/components/ProfileNavbar.tsx
@@ -19,13 +19,16 @@ const ProfileNavbar = () => {
   return (
     <Box className='pt-[6rem] pb-6 drop-shadow-2xl text-center'>
       <Flex flexDir='column' gap={4}>
-        {account.isConnected && navItems.map((item, i) => (
-          <Link key={i} className={classNames(`py-6  hover:bg-purple-700`, {
-            'bg-purple-700 text-white font-semibold': pathname === item.href
-          })} href={item.href}>
-            {item.name}
-          </Link>
-        ))}
+        <Link className={classNames(`py-6 transition-all duration-150  hover:bg-purple-600`, {
+          'bg-purple-700 text-white font-semibold': pathname === '/profile'
+        })} href='/profile'>
+          My Profile
+        </Link>
+        <Link className={classNames(`py-6 transition-all duration-150  hover:bg-purple-600`, {
+          'bg-purple-700 text-white font-semibold': pathname === '/profile/market'
+        })} href='/profile/market'>
+          Market Place
+        </Link>
         <Link href='/profile/uploadcontent'>
           <ButtonType className={classNames(` `, {
             'bg-purple-700 text-white font-semibold': pathname === '/profile/uploadcontent'
