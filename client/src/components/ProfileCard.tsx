9efeca1e167adbd4cf2f181bee62d4ebeb148a4b
Ebuka <ebuka2264@yahoo.com>
2023-07-24 16:50:25 +0100

    added payment

--- client/src/components/ProfileCard.tsx
+++ client/src/components/ProfileCard.tsx
@@ -3,6 +3,14 @@
 import { Badge, Box, Card, CardBody, Flex, Text } from '@chakra-ui/react';
 import Image, { StaticImageData } from 'next/image';
 import { IconType } from 'react-icons';
+import { getAccount, getWalletClient,getNetwork } from '@wagmi/core'
+import { prepareWriteContract, writeContract, waitForTransaction } from '@wagmi/core'
+import { contractAddresses, abi } from "../../constants"
+import{ethers} from "ethers"
+
+
+//import { gateway } from "../utils/payment"
+
 
 interface ProfileCardProps {
   image: StaticImageData,
@@ -14,8 +22,35 @@ interface ProfileCardProps {
   price?: string
   icon?: IconType
 }
-
 const ProfileCard = ({ image, title, description, wallet, walletAddress, badge, price, icon: Icon }: ProfileCardProps) => {
+  const account = getAccount()
+  
+  /*{ts-ignore}**/
+
+  async function gateway() {
+    
+    const { chain, chains } = getNetwork()
+    const walletClient = await getWalletClient()
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const config = await prepareWriteContract({
+      //@ts-ignore
+      address: contractAddresses[chain?.id][0], 
+      abi: abi,
+      functionName: 'pay',
+      args: [0, "0xf9808e0a01C513720e7878cF4Ca719ec53310fD8", '0'],
+      value: BigInt(100000),
+    })        
+    const { hash } = await writeContract(config)
+    const data =  await waitForTransaction({
+      confirmations: 1,
+      hash,
+    })
+    if (data.status == 'success') {
+      // CALL ANN'S API HERE
+      console.log(data);
+    }
+  }
+
   return (
     <>
       <Card mb={8}>
@@ -31,7 +66,11 @@ const ProfileCard = ({ image, title, description, wallet, walletAddress, badge, 
               <Text textAlign='end' as='strong'>{walletAddress}</Text>
             </Flex>
             <Text>{description}</Text>
-            <Flex className=' items-center gap-1 text-purple-700 text-md cursor-pointer w-fit' >
+            <Flex className=' items-center gap-1 text-purple-700 text-md cursor-pointer w-fit' onClick={async () => {
+              account.isConnected ?
+                await gateway()
+                :alert("Please Connect your Wallet")
+            }} >
               <Text>{price}</Text>
               {Icon && <Icon size={24} />}
             </Flex>
--- client/src/utils/payment.js
+++ client/src/utils/payment.js
@@ -1,19 +1,21 @@
-import { useContractWrite, usePrepareContractWrite } from 'wagmi'
-import { contractAddresses, abi } from "../constants"
-import { getNetwork } from '@wagmi/core'
-import { getChainId } from 'viem/public'
+// import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+// import { contractAddresses, abi } from "../constants"
+// import { getNetwork } from '@wagmi/core'
+// import { getChainId } from 'viem/public'
 
-import { ethers } from "ethers";
-//import { getChainId } from 'viem/dist/types/actions/public/getChainId';
+// import { ethers } from "ethers";
+// //import { getChainId } from 'viem/dist/types/actions/public/getChainId';
 
 
-export const Gateway = (address, amount) => {
-  const { chain, chains } = getNetwork()
-  const chainId = chain?.id
-    const { config } = usePrepareContractWrite({
-        address: contractAddresses[getChainId][0], //chain?.id
-        abi: abi,
-        functionName: 'pay',
-    })
-  const { data, isLoading, isSuccess, write } = useContractWrite(config)
-}
+// export const gateway = (address, amount) => {
+//   const { chain, chains } = getNetwork()
+//   const chainId = chain?.id
+//     const { config } = usePrepareContractWrite({
+//         address: contractAddresses[getChainId][0], //chain?.id
+//         abi: abi,
+//         functionName: 'pay',
+        
+//     })
+//   const { data, isLoading, isSuccess, write } = useContractWrite(config)
+//   return(data,isLoading,isLoading,write)
+// }
