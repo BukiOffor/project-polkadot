a9e0b2bcccd5bf32775990fad3f08a51ea138add
CEN Smart <echinnannachukwuemeka@gmail.com>
2023-07-24 11:40:18 +0100

    new update

--- client/src/components/FileUpload.tsx
+++ client/src/components/FileUpload.tsx
@@ -6,7 +6,7 @@ const FileUpload = () => {
     <>
       <Flex className=' items-center justify-center flex-col gap-2' >
         <Input className=' w-[7.8rem] outline-none border-none' id='file-input' name='file-input' type='file' />
-        <FormLabel htmlFor='file'>Click to upload an image or drag here</FormLabel>
+        <FormLabel color='gray.500' fontSize='sm' textAlign='center' htmlFor='file'>Click to upload an image or drag here</FormLabel>
       </Flex>
     </>
   )
