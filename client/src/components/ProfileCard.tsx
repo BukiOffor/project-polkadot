c63c7c84a63dfe0111564ca1d3e5250e7328191a
Ebuka <ebuka2264@yahoo.com>
2023-07-25 10:53:49 +0100

    gateway bug

--- client/src/components/ProfileCard.tsx
+++ client/src/components/ProfileCard.tsx
@@ -29,6 +29,49 @@ interface ProfileCardProps {
 
 const ProfileCard = ({ image, title, description, wallet, walletAddress, badge, price, icon: Icon, hover, onClick }: ProfileCardProps) => {
 
+  const account = getAccount()
+  const notification = useNotification()
+
+  async function gateway() {
+    const { chain, chains } = getNetwork()
+    const walletClient = await getWalletClient()
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    //const price = await priceFeed()
+    const config = await prepareWriteContract({
+      //@ts-ignore
+      address: '0x771ABC39aA6dDa99D3b3cBb8ea63B19b7472f68c', //contractAddresses[chain?.id][0] 
+      abi: abi,
+      functionName: 'pay',
+      args: [0, "0xf9808e0a01C513720e7878cF4Ca719ec53310fD8", BigInt(0.2 * 1e18)],
+      value: BigInt(ethers.utils.parseEther("0.2").toString()),
+    })        
+    const { hash } = await writeContract(config)
+    notification.show({
+      message: 'Wait while your transaction is being processed',
+      title: 'Transacrion Status',
+      variant: 'info'
+    })   
+    const data =  await waitForTransaction({
+      confirmations: 1,
+      hash,
+    })
+    if (data.status == 'success') {
+      // CALL ANN'S API HERE
+      console.log(data);
+      notification.show({
+        message: 'Your purchase was succesfull',
+        title: 'Transaction Status',
+        variant: 'success'
+      })   
+    } else {
+      notification.show({
+        message: 'Your purchase could not be processed', 
+        title: 'Transaction Status',
+        variant: 'error'
+    })
+    }
+  }
+  
   return (
     <>
       <Card onClick={onClick} _hover={{
@@ -50,11 +93,11 @@ const ProfileCard = ({ image, title, description, wallet, walletAddress, badge, 
             </Flex>
             <Text>{description}</Text>
             <Flex className=' items-center gap-1 text-purple-700 text-md cursor-pointer w-fit'>
-              {/* onClick={async () => {
+              {onClick={async () => {
                 account.isConnected ?
                   await gateway()
                   : alert("Please Connect your Wallet")
-              }} */}
+              }} }
               <Text>{price}</Text>
               {Icon && <Icon size={24} />}
             </Flex>
