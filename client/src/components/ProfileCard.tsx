faa19d11455ca6d91761f9a903602f3e66d866c4
Ebuka <ebuka2264@yahoo.com>
2023-07-25 11:59:07 +0100

    fixes gatway

--- client/src/components/ProfileCard.tsx
+++ client/src/components/ProfileCard.tsx
@@ -92,12 +92,11 @@ const ProfileCard = ({ image, title, description, wallet, walletAddress, badge, 
               <Text textAlign='end' as='strong'>{walletAddress}</Text>
             </Flex>
             <Text>{description}</Text>
-            <Flex className=' items-center gap-1 text-purple-700 text-md cursor-pointer w-fit'>
-              {onClick={async () => {
-                account.isConnected ?
-                  await gateway()
-                  : alert("Please Connect your Wallet")
-              }} }
+            <Flex className=' items-center gap-1 text-purple-700 text-md cursor-pointer w-fit' onClick={async () => {
+              account.isConnected ?
+                await gateway()
+                :alert("Please Connect your Wallet")
+            }} >
               <Text>{price}</Text>
               {Icon && <Icon size={24} />}
             </Flex>
@@ -109,3 +108,4 @@ const ProfileCard = ({ image, title, description, wallet, walletAddress, badge, 
 }
 
 export default ProfileCard
+
